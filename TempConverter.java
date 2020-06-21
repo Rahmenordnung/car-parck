0ef04415ca3a9db44d63f14cb262fce12242266c
gitlassian <nialldeely@gmail.com>
2020-06-21 15:03:12 +0100

    Add files via upload

--- TempConverter.java
+++ TempConverter.java
@@ -3,18 +3,20 @@ package com.mycompany.jerseytutorial;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/temp")
 public class TempConverter {
-    @GET
+    String output = "";
+    double tempValue;
+    double convertedTemp;
+
+    //@GET
     //tempType is "fahrenheit" or "celsius", tempVal is String temperature
-    @Path("/{tempType}/{tempVal}")
-    public Response convert(@PathParam("tempType") String tempType, @PathParam("tempVal") String temp) {
-        String output = "";
-        double tempValue;
-        double convertedTemp;
-        
+    //@Path("/{tempType}/{tempVal}")
+    public Response convert(String tempType, String temp) {
         //catch numberExceptions when setting tempValue
         try { tempValue = Double.parseDouble(temp); }
         catch (NumberFormatException e) {
@@ -41,4 +43,31 @@ public class TempConverter {
         }
         return Response.status(200).entity(output).build();
     }
+    
+    //curl -vi -H "Accept: text/plain" -X GET -G "http://localhost:49000/api/temp/celsius/32.2"
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/{tempType}/{tempVal}")
+        public Response getTempPlain(@PathParam("tempType") String tempType, @PathParam("tempVal") String temp) { 
+            Response r1 = convert(tempType, temp);
+            return r1;
+        }
+
+    //curl -vi -H "Accept: text/xml" -X GET -G "http://localhost:49000/api/temp/celsius/32.2"
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    @Path("/{tempType}/{tempVal}")
+        public String getTempXML(@PathParam("tempType") String tempType, @PathParam("tempVal") String temp) { 
+            Response r1 = convert(tempType, temp);
+            return "<?xml version=\"1.0\"?>" + "<temperature>" + output + "</temperature>";
+        }
+
+    //curl -vi -H "Accept: text/html" -X GET -G "http://localhost:49000/api/temp/celsius/32.2"
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("/{tempType}/{tempVal}")
+        public String getTempHTML(@PathParam("tempType") String tempType, @PathParam("tempVal") String temp) { 
+            Response r1 = convert(tempType, temp);
+            return "<html><title>Temperature Conversion</title><body><h1>Temperature Conversion</h1><p>" + output + "</p></body></html>";
+        }
 }
